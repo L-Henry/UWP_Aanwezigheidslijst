f57722d15a392fafddc32425e6fb8bc0370cb97c
L-Henry <Henry.Ludwig@telenet.be>
2019-08-12 15:23:01 +0200

    Add project files.

---
+++ UWP_Aanwezigheidslijst.EF/AanwezigheidslijstDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using UWP_Aanwezigheidslijst.EF.Model;
+
+namespace UWP_Aanwezigheidslijst.EF
+{
+    public class AanwezigheidslijstDBContext : DbContext
+    {
+        public DbSet<Docent> Docenten { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Date Scourse = aanwezigheidslijst.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Docent>()
+                .ToTable("Docenten")
+                .HasKey(d => d.Id);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
